01575fc882f9ded07991e317d68a9b820642d1d8
Abhishek <abhishekup082@gmail.com>
2026-04-08 12:13:49 +0530

    chore(deps): replace mitchellh/mapstructure with go-viper/mapstructure/v2
    Replace the archived mitchellh/mapstructure with the maintained go-viper/mapstructure/v2 fork and bump score-go to v1.13.0 which includes the same migration.
    
    Signed-off-by: Abhishek <abhishekup082@gmail.com>

--- internal/provisioners/templateprov/template.go
+++ internal/provisioners/templateprov/template.go
@@ -24,7 +24,7 @@ import (
 	"text/template"
 
 	"github.com/Masterminds/sprig/v3"
-	"github.com/mitchellh/mapstructure"
+	"github.com/go-viper/mapstructure/v2"
 	"github.com/score-spec/score-go/framework"
 	"gopkg.in/yaml.v3"
 	"k8s.io/apimachinery/pkg/runtime"
